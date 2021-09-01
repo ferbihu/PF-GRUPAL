7caf1f361406477b553d791e9f2a1f3bdbb42242
V33RO <cardozoveronicaro@gmail.com>
2021-09-01 19:50:36 -0300

    mapadd1

--- client/src/Pages/LugaresSeguros.jsx
+++ client/src/Pages/LugaresSeguros.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { MapInit } from "../Components/Mapa/Map";
 
 export default function LugaresSeguros() {
     return (
         <div>
             <h1>Hola, soy Lugares Seguros</h1>
+             <MapInit/>
         </div>
     )
 }
