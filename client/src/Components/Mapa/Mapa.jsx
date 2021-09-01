ebb736bf365a5271b15ce0cefc9b7ff496a99af7
Mora <morabessone29@gmail.com>
2021-09-01 11:48:37 -0300

    lugares seguros

--- client/src/Components/Mapa/Mapa.jsx
+++ client/src/Components/Mapa/Mapa.jsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
+import mapa from "/home/mora/Escritorio/Pf grupal/client/src/imgs/wmn (1).png"
 
-
-
-export default function Mapa() {
-
-  return (
-    <div>
-      HOLAAA MAP
-    </div>
-  );
+const Mapa = () => {
+    return (
+        <div>
+            <h1>Lugares Seguros</h1>
+              <Link to="/lugaresseguros">
+                <img src={mapa} alt="Not found"/>
+              </Link>
+            <p>Encontrá los puntos de la ciudad a los que podes acudir si te encontras en una situación que no te sentis cómoda.</p>
+        </div>
+    )
 }
+
+export default Mapa; 
