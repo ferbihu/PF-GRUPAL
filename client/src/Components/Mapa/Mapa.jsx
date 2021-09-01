a54a4da5e2734c084b2883c6a9c751ec64b18d1b
Florencia Ferrari <ferrariflorenciab@gmail.com>
2021-09-01 12:29:22 -0300

    cambiosestilos

--- client/src/Components/Mapa/Mapa.jsx
+++ client/src/Components/Mapa/Mapa.jsx
@@ -1,18 +1,18 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import mapa from "/home/mora/Escritorio/Pf grupal/client/src/imgs/wmn (1).png";
+import mapa from "../../imgs/wmn (1).png";
 import "./Mapa.css";
 
 const Mapa = () => {
-    return (
-        <div>
-            <h1>Lugares Seguros</h1>
-              <Link to="/lugaresseguros">
-                <img src={mapa} alt="Not found" className="img"/>
-              </Link>
-            <p>Encontrá los puntos de la ciudad a los que podes acudir si te encontras en una situación que no te sentis cómoda.</p>
-        </div>
-    )
+  return (
+    <div className="container">
+      <h1>Lugares Seguros</h1>
+      <Link to="/lugaresseguros">
+        <img src={mapa} alt="Not found" className="img" />
+      </Link>
+      <p>Encontrá los puntos de la ciudad a los que podes acudir si te encontras en una situación que no te sentis cómoda.</p>
+    </div>
+  )
 }
 
-export default Mapa; 
+export default Mapa;
