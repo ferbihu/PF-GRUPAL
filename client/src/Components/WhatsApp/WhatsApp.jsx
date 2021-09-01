fb5836bf08a35f8eb1d2a3f004331676cb207da1
AgustinaMartinez4 <agustina-martinez@live.com.ar>
2021-09-01 14:22:10 -0300

    “WsapVentana”

--- client/src/Components/WhatsApp/WhatsApp.jsx
+++ client/src/Components/WhatsApp/WhatsApp.jsx
@@ -4,13 +4,14 @@ import './WhatsApp.css';
 
 
 export default function Foro(){
+    
    return (
        <div  className='fondoWhatsapp'>
            <div className='titulo144'>
            <h3>Comunicate con el 144</h3>
            </div>
-           <a href="https://api.whatsapp.com/send?phone=5491127716463">
-              <button className='btnayuda' >BOTÓN DE AYUDA</button>
+            <a href="javascript:window.open('https://api.whatsapp.com/send?phone=5491127716463','','width=600,height=400,left=50,top=50,toolbar=yes');void 0"><br />
+              <button className='btnayuda'>BOTÓN DE AYUDA</button>
           </a> 
        </div>
    )
