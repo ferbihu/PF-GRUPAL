c079d4313783a8f30941220c8b8c6896bc0dc5f9
AgustinaMartinez4 <agustina-martinez@live.com.ar>
2021-09-01 13:48:07 -0300

    “masEstilos”

--- client/src/Components/WhatsApp/WhatsApp.jsx
+++ client/src/Components/WhatsApp/WhatsApp.jsx
@@ -6,7 +6,9 @@ import './WhatsApp.css';
 export default function Foro(){
    return (
        <div  className='fondoWhatsapp'>
+           <div className='titulo144'>
            <h3>Comunicate con el 144</h3>
+           </div>
            <a href="https://api.whatsapp.com/send?phone=5491127716463">
               <button className='btnayuda' >BOTÓN DE AYUDA</button>
           </a> 
