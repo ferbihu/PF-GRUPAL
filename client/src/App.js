7755233c14f23b1c9cb10179e18ca91e10a99e87
AgustinaMartinez4 <agustina-martinez@live.com.ar>
2021-08-31 23:12:40 -0300

    CambiosEnApp

--- client/src/App.js
+++ client/src/App.js
@@ -8,20 +8,6 @@ function App() {
     <div className="App">
       <Switch>
       <Route exact path="/" component={Landing}/>
-      {/* <header className="App-header">
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       </Switch>
     </div>
     </BrowserRouter>
