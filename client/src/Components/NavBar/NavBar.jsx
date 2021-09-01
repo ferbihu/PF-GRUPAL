a2a4de0bc98dc54e3915a63319b0285945265a90
Mora <morabessone29@gmail.com>
2021-09-01 13:26:19 -0300

    fuentes y estilos

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,26 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./NavBar.css";
 
 const Nav = () => {
     return (
-        <div>
+        <div className="conteiner">
             <Link to="/">
-                <h1>S.</h1>
+                <h1 className="s">S.</h1>
             </Link>
             <Link to = "/conocenos">
+                <p className="text">
                 Conocenos
+                </p>
             </Link>
             <Link to = "/foro">
+                <p className="text">
                 Foro
+                </p>
             </Link>
             <Link to = "/lugaresseguros">
+                <p className="text">
                 Lugares seguros
+                </p>
             </Link>
             <Link to = "/iniciasesion">
+                <p className="text">
                 Iniciar sesión
+                </p>
             </Link>
             <Link to = "/registrate">
+                <p className="text">
                 Registrate
+                </p>
             </Link>
         </div>
     )
