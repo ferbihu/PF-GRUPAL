e310e63d0a01e2821b9d868f1f0428016269a065
morabessone <79546017+morabessone@users.noreply.github.com>
2021-09-02 11:46:30 -0300

    componente Login (#26)

--- client/src/Components/Mapa/Mapa.jsx
+++ client/src/Components/Mapa/Mapa.jsx
@@ -9,7 +9,7 @@ export default function Mapa() {
 
   return (
     <div className="container">
-      <h1>Lugares Seguros</h1>
+      <h1 className="txt">LUGARES SEGUROS</h1>
       <Link to="/lugaresseguros">
         <img src={mapa} alt="Not found" className="img" />
       </Link>
--- client/src/Pages/IniciaSesion.jsx
+++ client/src/Pages/IniciaSesion.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import "./IniciaSesion.css"
 
 export default function IniciaSesion() {
     return (
-        <div>
-            <h1>Hola, soy Inicia Sesión</h1>
-        </div>
+        // <div>
+        //     <h1 className="txtLog">Inicia sesión</h1>
+        //     <label>Email</label>
+        //     <input placeholder="nombre@ejemplo.com"/>
+        //     <label>Contraseña</label>
+        //     <input placeholder="Más de 6 caracteres"/>
+        //     <button>Ingresá</button>
+        // </div>
+            <div className="contenedor-form">
+                <h1 className="txtLog">Inicia sesión</h1>
+                <form class="login-form">
+                    <label className="p">Email</label>
+                    <input className="login-username" placeholder="nombre@example.com" />
+                    <label className="p2">Contraseña</label>
+                    <input className="login-password" placeholder="Más de 6 caracteres" />
+                    <button className="login-submit">Ingresá</button>
+                </form>
+            </div>
+
     )
 }
