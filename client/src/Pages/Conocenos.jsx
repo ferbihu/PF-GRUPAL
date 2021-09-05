9bd23df13986cc544f57d0f2fa8524d0717e1c1c
morabessone <79546017+morabessone@users.noreply.github.com>
2021-09-05 18:59:55 -0300

    Conocenos (#62)
    * nueva rama
    
    * page conocenos

--- client/src/Pages/Conocenos.jsx
+++ client/src/Pages/Conocenos.jsx
@@ -3,7 +3,28 @@ import React from "react";
 export default function Conocenos() {
     return (
         <div>
-            <h1>Hola, soy Conocenos</h1>
+            <h2>Conocenos</h2>
+            <div>
+            <p>Safety es una plataforma de geolocalización desarrollada con el fin de implicar al conjunto de la sociedad en la lucha contra la violencia de género, y promover información sobre cómo actuar ante un caso de acoso o violencia contra las mujeres.
+            Los lugares públicos, como bares, restaurantes y estaciones de servicio podrán adherirse a la causa registrándose como un lugar seguro. 
+            Estos puntos tendrán un protocolo para manejar las distintas situaciones que podrían presentarse. 
+            La aplicación trabaja con la ubicación en vivo de las usuarias para poder acudir a los lugares seguros cercanos a su ubicación.</p>
+            <h3>Desarrollada por</h3>
+            <br/>
+            <p>Mora Bessone</p>
+            <br/>
+            <p>Florencia Ferrari</p>
+            <br/>
+            <p>Fernanda Bihurriet</p>
+            <br/>
+            <p>Agustina Martínez</p>
+            <br/>
+            <p>Anyel Dayana</p>
+            <br/>
+            <p>Carolina</p>
+            <br/>
+            <p>Verónica Cardozo</p>
+            </div>
         </div>
     )
 }
