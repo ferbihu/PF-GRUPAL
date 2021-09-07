494cd7ba57dee09b603f7041306d8ca7a1126fb9
Mora <morabessone29@gmail.com>
2021-09-07 14:59:16 -0300

    nodeMailer funcionando

--- api2/src/routes/nodemailer.js
+++ api2/src/routes/nodemailer.js
@@ -1,17 +1,26 @@
 const { transporter } = require("../utils/index");
+const express = require("express");
+const router = require("express").Router();
+router.use(express.json());
+const { GMAIL } = process.env;
 
-const sendMailNewUser = async (req, res, next) => {
-    var { email } = req.body;
-    email = email.toLowerCase();
-    await transporter.sendMail({
-        from: `Safety <womanforwoman15@gmail.com>`,
-        to: email,
-        subject: "Bienvenido a Safety!",
-        text: "Hola! Nos alegra que te hayas incorporado a la comunidad Safety. A continuación te enviamos un PDF con información sobre nuestro sitio. Podes ponerte en contacto con nosotras escribiendonos a women4women@gmail.com en caso de necesitar ayuda con algo.",
-        html: `<p>Hola! Nos alegra que te hayas incorporado a la comunidad Safety. A continuación te enviamos un PDF con información sobre nuestro sitio. Podes ponerte en contacto con nosotras escribiendonos a women4women@gmail.com en caso de necesitar ayuda con algo.</p>`
-    })
-}
 
-module.exports = {
-    sendMailNewUser
-}
+router.post("/welcome", async (req, res, next) => {
+    try {
+        var {email} = req.body;
+        console.log("aca esta email", email)
+        await transporter.sendMail({
+            from: `"Safety" <${GMAIL}>`,
+            to: email,
+            subject: "Bienvenido a Safety!",
+            text: "Hola! Nos alegra que te hayas incorporado a la comunidad Safety. A continuación te enviamos un PDF con información sobre nuestro sitio. Podes ponerte en contacto con nosotras escribiendonos a women4women@gmail.com en caso de necesitar ayuda con algo.",
+            html: `<p>Hola! Nos alegra que te hayas incorporado a la comunidad Safety. A continuación te enviamos un PDF con información sobre nuestro sitio. Podes ponerte en contacto con nosotras escribiendonos a women4women@gmail.com en caso de necesitar ayuda con algo.</p>`
+        })
+        res.json({ success: "Email sent" });
+    } catch(err) {
+        console.log(err.message);
+    }
+})
+
+
+module.exports = router;
