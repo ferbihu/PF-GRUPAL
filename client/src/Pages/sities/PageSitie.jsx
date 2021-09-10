fad5721c99effcf0780fac07773ccd2ea4965f83
V33RO <cardozoveronicaro@gmail.com>
2021-09-10 10:47:44 -0300

    pagina

--- client/src/Pages/sities/PageSitie.jsx
+++ client/src/Pages/sities/PageSitie.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import "./PageSitie.css"
+import "./PageSitie.css";
+import {Map} from "../../Components/Mapa/Map";
+import {Link} from 'react-router-dom';
 
 export default function Sities() {
     return (
@@ -8,7 +10,7 @@ export default function Sities() {
             <div className="app-header">
                 <div className="app-header">
                   <span className="app-icon"></span>
-                    <p className="app-name">Lugares</p>
+                    <p className="app-name">YOUR SAFETY</p>
                     <div className="search-wrapper">
                         <input className="search-input" type="text" placeholder="Search"/>
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="app-search" viewBox="0 0 24 24">
@@ -79,11 +81,82 @@ export default function Sities() {
                     </svg>
                   </a>
               </div>
-              <div>
-                    LUGARES SEGUROS MAS CERCANOS
-                      <div>
-                          
+               <div>
+                #
+                      <div className="mapa">
+                          <Map/>
+                          <Link to='/registratelugarseguro'><button className="btnainput">Registrá un lugar seguro</button></Link>
                       </div>
+                      
+                </div>
+
+                <div>
+                <div className="messages-section">
+               <button className="messages-close">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-x-circle">
+                    <circle cx="12" cy="12" r="10" />
+                    <line x1="15" y1="9" x2="9" y2="15" />
+                    <line x1="9" y1="9" x2="15" y2="15" />
+                  </svg>
+                </button>
+                <div className="projects-section-header">
+                   <p>Comentarios</p>
+                </div>
+                <div className="messages">
+                    <div className="message-box">
+                    <img className="imagebox" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80" alt="profile image"/>                        
+                    <div className="message-content">
+                    <div className="message-header">
+                        <div className="name">Stephanie</div>
+                           <div className="star-checkbox">
+                             <input type="checkbox" id="star-1"/>
+                                <label for="star-1">
+                                   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-star">
+                                      <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
+                                   </svg>
+                                </label>
+                            </div>
+                        </div>
+                         <p className="message-line">
+                           I got your first assignment. It was quite good. 🥳 We can continue with the next 
+                         </p>
+                         <p className="message-line time">
+                           Dec, 12
+                         </p>
+                    <div className="message-box">
+                    <img className="imagebox"src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80" alt="profile image"/>
+                      <div className="message-content">
+                         <div className="message-header">
+                            <div className="name">Mark</div>
+                            <div className="star-checkbox">
+                                <input type="checkbox" id="star-2"/>
+                                <label for="star-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-star">
+                                    <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
+                                    </svg>
+                                </label>
+                            </div>
+                         </div>
+                         <p className="message-line">
+                           Hey, can tell me about progress of project? I'm waiting for your response.
+                        </p>
+                        <p className="message-line time">
+                           Dec, 12
+                        </p>
+                      </div>
+                    
+                    </div>
+                    
+                    
+                    </div>
+                     
+
+
+                    
+                    </div>
+                </div>
+                
+            </div>
                 </div>
             </div>
             <div className="projects-section">
@@ -212,71 +285,7 @@ export default function Sities() {
                   </div>
                  </div>
                 </div>    
-            <div className="messages-section">
-               <button className="messages-close">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-x-circle">
-                    <circle cx="12" cy="12" r="10" />
-                    <line x1="15" y1="9" x2="9" y2="15" />
-                    <line x1="9" y1="9" x2="15" y2="15" />
-                  </svg>
-                </button>
-                <div className="projects-section-header">
-                   <p>Comentarios</p>
-                </div>
-                <div className="messages">
-                    <div className="message-box">
-                    <img className="imagebox" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80" alt="profile image"/>                        
-                    <div className="message-content">
-                    <div className="message-header">
-                        <div className="name">Stephanie</div>
-                           <div className="star-checkbox">
-                             <input type="checkbox" id="star-1"/>
-                                <label for="star-1">
-                                   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-star">
-                                      <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
-                                   </svg>
-                                </label>
-                            </div>
-                        </div>
-                         <p className="message-line">
-                           I got your first assignment. It was quite good. 🥳 We can continue with the next 
-                         </p>
-                         <p className="message-line time">
-                           Dec, 12
-                         </p>
-                    <div className="message-box">
-                    <img className="imagebox"src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80" alt="profile image"/>
-                      <div className="message-content">
-                         <div className="message-header">
-                            <div className="name">Mark</div>
-                            <div className="star-checkbox">
-                                <input type="checkbox" id="star-2"/>
-                                <label for="star-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-star">
-                                    <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
-                                    </svg>
-                                </label>
-                            </div>
-                         </div>
-                         <p className="message-line">
-                           Hey, can tell me about progress of project? I'm waiting for your response.
-                        </p>
-                        <p className="message-line time">
-                           Dec, 12
-                        </p>
-                      </div>
-                    
-                    </div>
-                    
-                    
-                    </div>
-                     
-
-
-                    
-                    </div>
-                </div>
-            </div>
+           
             
             </div>
             
