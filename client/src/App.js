0546f93c7566e568f5f4dd560b4386fb613aa56b
Qarola <67078790+Qarola@users.noreply.github.com>
2021-09-11 09:21:25 -0500

    Estilos Perfil de Usuario (#98)

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Landing from './Pages/Landing';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from './Pages/User-Profile-Page/helpers/index';
 import './App.css';
 //Import Pages
 import Footer from "./Components/Footer/Footer.jsx";
@@ -10,6 +12,7 @@ import Conocenos from "./Pages/Conocenos";
 import IniciaSesion from "./Pages/IniciaSesion";
 import RegistrateLugarSeguro from "./Pages/RegistrateLugarSeguro";
 import LugaresSeguros from "./Pages/LugaresSeguros";
+import MainProfile from "./Pages/User-Profile-Page/MainProfile";
 import Registrate from "./Pages/Registro-Usuario/Registrate";
 import Nav from "./Components/NavBar/NavBar";
 import Panel from "./Pages/Panel-Admin/Panel";
@@ -19,6 +22,7 @@ function App() {
 
   return (
     <BrowserRouter>
+     <ChakraProvider theme={theme}>
     <div className="App">
       <Nav/>
       <Switch>
@@ -27,12 +31,14 @@ function App() {
       <Route exact path="/lugaresseguros" component={LugaresSeguros}/>
       <Route exact path="/registratelugarseguro" component={RegistrateLugarSeguro}/>
       <Route exact path="/iniciasesion" component={IniciaSesion}/>
+      <Route exact path="/profile" component={MainProfile} />
       <Route exact path="/registrate" component={Registrate}/>
       <Route exact path="/foro" component={Foro}/>
       <Route exact path="/panel" component={Panel}/>
       </Switch>
       <Footer/>
     </div>
+    </ChakraProvider>
     </BrowserRouter>
   );
 }
