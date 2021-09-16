2c56c7cf53fe0f5af61a4ddb71e5ccd98107cc09
AgustinaMartinez4 <agustina-martinez@live.com.ar>
2021-09-16 13:11:26 -0300

    sinerrores

--- client/src/Components/ForoNoticias/ForoNoticias.jsx
+++ client/src/Components/ForoNoticias/ForoNoticias.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import React from "react";
 import notamujeres from "../../imgs/notamujeres.png";
 import "./ForoNoticias.css";
 
