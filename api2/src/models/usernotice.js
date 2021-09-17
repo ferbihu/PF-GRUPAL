31d369f7be2ee42e60c9ecf558887f51eb7f147e
V33RO <cardozoveronicaro@gmail.com>
2021-09-17 08:39:32 -0300

    sinlugares

--- api2/src/models/usernotice.js
+++ api2/src/models/usernotice.js
@@ -4,6 +4,11 @@ const  { sequelize } = require('../db');
 
 module.exports = function (sequelize) {
     return sequelize.define('usernotice', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
         title: {
             type: DataTypes.TEXT,
             allowNull: false,
