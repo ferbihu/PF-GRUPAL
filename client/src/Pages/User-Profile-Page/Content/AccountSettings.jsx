0ef5c544c496e447480b29aefd70c9202db145c2
Qarola <67078790+Qarola@users.noreply.github.com>
2021-09-17 15:20:39 -0500

    Navbar para user profile (#167)

--- client/src/Pages/User-Profile-Page/Content/AccountSettings.jsx
+++ client/src/Pages/User-Profile-Page/Content/AccountSettings.jsx
@@ -4,7 +4,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import MySafePlace from "../../../Components/PlaceAprobation/MySafePlace";
 
-
 import {
   FormControl,
   FormLabel,
@@ -14,7 +13,6 @@ import {
   Box,
   Button,
 } from "@chakra-ui/react";
-
 
 export default function AccountSettings() {
   const dispatch = useDispatch();
@@ -28,63 +26,63 @@ export default function AccountSettings() {
   //const[phone, setPhone] = React.useState('')
   //const handleChangeName = (event) => setName(event.target.value)
   //const handleChangeLastname = (event) => setLastname(event.target.value)
-  let lugaresSeguros = useSelector((state) => state.stateSitie) //estado con todos los lugares seguros aceptados
-  let id_usuario = useSelector((state) => state.userId) // me traigo el id del usuario que esta registrado
-  console.log(id_usuario)
-  let isLogged = useSelector((state) => state.isLogged)
+  let lugaresSeguros = useSelector((state) => state.stateSitie); //estado con todos los lugares seguros aceptados
+  let id_usuario = useSelector((state) => state.userId); // me traigo el id del usuario que esta registrado
+  console.log(id_usuario);
+  let isLogged = useSelector((state) => state.isLogged);
   const [input, setInput] = useState({
     name: "",
     lastname: "",
     country: "",
     town: "",
     phone: "",
-
-  })
+  });
 
   const handleInputChange = (e) => {
-    e.preventDefault()
-    console.log(e.target.value)
+    e.preventDefault();
+    console.log(e.target.value);
     setInput({
       ...input,
-      [e.target.name]: e.target.value
-    })
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
 
   const handleSelectCountry = (e) => {
-    console.log(e.target.value)
-    setInput({ ...input, country: e.target.value })
+    console.log(e.target.value);
+    setInput({ ...input, country: e.target.value });
   };
 
   const handleSelectTown = (e) => {
-    console.log(e.target.value)
-    setInput({ ...input, town: e.target.value })
+    console.log(e.target.value);
+    setInput({ ...input, town: e.target.value });
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     if (input.name && input.lastname) {
       dispatch(updateDataUser(id_usuario, input));
-      alert('Se han actualizados sus datos')
+      alert("Se han actualizados sus datos");
     }
-  }
+  };
   // history.push('/')
 
-
-  let lugaresSegurosFiltrados = lugaresSeguros.filter(e => e.userId === id_usuario)
-  console.log(lugaresSegurosFiltrados)
+  let lugaresSegurosFiltrados = lugaresSeguros.filter(
+    (e) => e.userId === id_usuario
+  );
+  console.log(lugaresSegurosFiltrados);
 
   useEffect(() => {
-    dispatch(getallsafesitie())
+    dispatch(getallsafesitie());
   }, [dispatch]);
-
 
   const handleLogout = (e) => {
     e.preventDefault();
     if (isLogged === true) {
-      isLogged = false
-      history.push('/iniciasesion')
+      isLogged = false;
+      history.push("/iniciasesion");
+      window.location.reload();
     }
-  }
+  };
 
   /* const handleDelete = (e) => {
     e.preventDefault();
@@ -94,51 +92,60 @@ export default function AccountSettings() {
 
   return (
     <Grid
-      templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }}
+      templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(2, 1fr)" }}
       gap={6}
     >
-      <FormControl method='POST' id="firstName">
+      <FormControl method="POST" id="firstName">
         <FormLabel>Nombres</FormLabel>
-        <Input focusBorderColor="brand.blue"
+        <Input
+          focusBorderColor="brand.blue"
           required
           type="text"
-          name='name'
+          name="name"
           placeholder="nombre"
           value={input.name}
-          onChange={(e) => handleInputChange(e)} />
+          onChange={(e) => handleInputChange(e)}
+        />
       </FormControl>
-      <FormControl id="lastName" >
+      <FormControl id="lastName">
         <FormLabel>Apellidos</FormLabel>
-        <Input focusBorderColor="brand.blue"
+        <Input
+          focusBorderColor="brand.blue"
           type="text"
-          name='lastname'
+          name="lastname"
           placeholder="apellido"
           value={input.lastname}
-          onChange={(e) => handleInputChange(e)} />
+          onChange={(e) => handleInputChange(e)}
+        />
       </FormControl>
-      <FormControl id="phoneNumber"  >
+      <FormControl id="phoneNumber">
         <FormLabel>Número de teléfono</FormLabel>
         <Input
           focusBorderColor="brand.blue"
           type="tel"
-          name='phone'
+          name="phone"
           value={input.phone}
           placeholder="número de telefóno"
-          onChange={(e) => handleInputChange(e)} />
+          onChange={(e) => handleInputChange(e)}
+        />
       </FormControl>
-      <FormControl id="emailAddress"  >
+      <FormControl id="emailAddress">
         <FormLabel>Email</FormLabel>
         <Input
           focusBorderColor="brand.blue"
           type="email"
-          name='email'
+          name="email"
           disabled={true}
           placeholder="suemail@email.com"
         />
       </FormControl>
-      <FormControl id="city"  >
+      <FormControl id="city">
         <FormLabel>Ciudad</FormLabel>
-        <Select focusBorderColor="brand.blue" onChange={e => handleSelectTown(e)} placeholder="Select city">
+        <Select
+          focusBorderColor="brand.blue"
+          onChange={(e) => handleSelectTown(e)}
+          placeholder="Select city"
+        >
           <option value="palermo">Palermo</option>
           <option value="recoleta">Recoleta</option>
           <option value="centro">Centro</option>
@@ -149,7 +156,11 @@ export default function AccountSettings() {
       </FormControl>
       <FormControl id="country ">
         <FormLabel>País</FormLabel>
-        <Select focusBorderColor="brand.blue" onChange={e => handleSelectCountry(e)} placeholder="Select country">
+        <Select
+          focusBorderColor="brand.blue"
+          onChange={(e) => handleSelectCountry(e)}
+          placeholder="Select country"
+        >
           <option value="Argentina" selected>
             Argentina
           </option>
@@ -161,13 +172,13 @@ export default function AccountSettings() {
         </Button>
       </Box>
       <Box mt={5} py={5} px={8} borderTopWidth={1} borderColor="brand.light">
-        <Button onClick={handleLogout}>
+        <Button onClick={handleLogout} value="Reload Page">
           Logout
         </Button>
       </Box>
       <Box>
         <h1>Mis lugares seguros</h1>
-        {lugaresSegurosFiltrados.map(i => {
+        {lugaresSegurosFiltrados.map((i) => {
           return (
             <Box>
               <MySafePlace
@@ -183,12 +194,10 @@ export default function AccountSettings() {
                 relation={i.relation}
                 id={i.id}
               ></MySafePlace>
-
             </Box>
-
           );
         })}
       </Box>
     </Grid>
-  )
+  );
 }
