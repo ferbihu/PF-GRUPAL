c078fd33ff7fbc2a8fbd031fcee7b1c10c602894
V33RO <cardozoveronicaro@gmail.com>
2021-09-17 12:56:36 -0300

    Maincomentarionoticia (#165)
    * sinlugares
    
    * rutasok

--- api2/src/app.js
+++ api2/src/app.js
@@ -30,7 +30,7 @@ server.use("/delete_safe_place", require("./routes/safeplace"))
 
 server.use('/user', require('./routes/user'));
 
-server.use("/comments", require("./routes/usernotice"))
+server.use("/comments", require("./routes/commentnotice"))
 
 server.use("/newNotice", require("./routes/newNotice"))
 
--- api2/src/controllers/commentnotice.js
+++ api2/src/controllers/commentnotice.js
@@ -1,15 +1,15 @@
-const ServicesComments = require ('../services/usernotice');
+const ServicesCommentNotice = require ('../services/commentnotice');
 
 
 const getComments = async (_req,res)=>{
-    res.json(await ServicesComments.getComments());
+    res.json(await ServicesCommentNotice.getComments());
 
 };
 
 
 const postComments = async (req,res) =>{
     try{
-        await ServicesComments.postComments(req.body)
+        await ServicesCommentNotice.postComments(req.body)
             res.status(200).json({success:true})
     }
     catch(error){
@@ -21,7 +21,7 @@ const postComments = async (req,res) =>{
 const changeStatusComments = async (req,res) =>{
     const {id,status} = req.params;
     const {description_status = ""} = req.body;
-    const statusChange = await ServicesComments.deleteComments({status,description_status},id);
+    const statusChange = await ServicesCommentNotice.deleteComments({status,description_status},id);
     res.status(200).json({success:true})
 };
 
