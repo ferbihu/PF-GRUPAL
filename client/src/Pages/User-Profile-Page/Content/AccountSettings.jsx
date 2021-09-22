fb85caba76f2c3e7dd3ccfcaacd65a2792316a2e
Mora <morabessone29@gmail.com>
2021-09-22 13:41:27 -0300

    sacar estados

--- client/src/Pages/User-Profile-Page/Content/AccountSettings.jsx
+++ client/src/Pages/User-Profile-Page/Content/AccountSettings.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { getallsafesitie,getUserById, deleteSafePlace } from "../../../actions/actions";
+import { getallsafesitie, deleteSafePlace } from "../../../actions/actions";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useHistory } from "react-router-dom";
 import "./AccountSetting.css"
@@ -15,71 +15,11 @@ export default function AccountSettings() {
   }, [name])
 
  
-  let lugaresSeguros = useSelector((state) => state.stateSitie) //estado con todos los lugares seguros aceptados
-  console.log(lugaresSeguros)
   let id_usuario = localStorage.getItem("userId") // me traigo el id del usuario que esta registrado
   let isLogged = localStorage.getItem("isLogged")
   let isAdmin = localStorage.getItem("isAdmin")
   const userDataById = useSelector((state) => state.userDataById);
   console.log(userDataById)
-
-
-
-//console.log("SOY USER DATAID",userDataById)
-  const [input, setInput] = useState({
-    name: userDataById?.name || "",
-    lastname:userDataById?.lastname || "",
-    country: userDataById?.country || "",
-    town: userDataById?.town || "",
-    phone: userDataById?.phone || "",
-
-  });
-
-  const handleInputChange = (e) => {
-    console.log("entre acaaaaaaaaaaa")
-    e.preventDefault()
-    console.log("HANDLE CHANGE INPUUUUUUT",e.target.value)
-    setInput({
-      ...input,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-
-  const handleSelectCountry = (e) => {
-    console.log(e.target.value);
-    setInput({ ...input, country: e.target.value });
-  };
-
-  const handleSelectTown = (e) => {
-    console.log(e.target.value);
-    setInput({ ...input, town: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-      dispatch(updateDataUser(id_usuario, input));
-      alert("Se han actualizados sus datos");
-  };
-
-
-
-  let lugaresSegurosFiltrados = lugaresSeguros.filter(e => e.userId !== id_usuario)
-
-
-  useEffect(() => {
-    dispatch(getallsafesitie());
-  }, [dispatch]);
-
-
-  useEffect(()=>{
-    console.log("ESTOY EN EL USE EFECT",id_usuario)
-    dispatch(getUserById(id_usuario))
-  },
-  // eslint-disable-next-line
-  [dispatch]);
-
 
 
   const handleLogout = (e) => {
@@ -95,14 +35,6 @@ export default function AccountSettings() {
     }
   };
 
-
-
-  const handleDelete = (e) => {
-    e.preventDefault();
-    dispatch(deleteSafePlace(lugaresSegurosFiltrados.id))
-
-  } 
-
   return (
     isAdmin === "true" ? 
     <div className="contenedorProfile">
@@ -115,19 +47,6 @@ export default function AccountSettings() {
         <Link to="/users">
           <h2>Administrar usuarios</h2>
         </Link>
-        <h3>Tus lugares seguros</h3>
-        {
-          lugaresSegurosFiltrados?.map((s) => (
-            <div className="contenedor-lugares">
-            <div>
-              <button onClick={handleDelete}>X</button>
-              <p>{s.name}</p>
-              <p>{s.country}</p>
-              <p>{s.keyword}</p>
-            </div>
-            </div>
-          ))
-        }
         <button onClick={handleLogout}>Cerrar sesión</button>
       </div>
     </div> :
@@ -135,19 +54,6 @@ export default function AccountSettings() {
       <div className="contenedorinfouser">
         <img src={imgUser} className="imgUsername" alt="Not found"/>
         <h1>Hola {userDataById?.name}!</h1>
-        <h3>Tus lugares seguros</h3>
-        {
-          lugaresSegurosFiltrados?.map((s) => (
-            <div className="contenedor-lugares">
-            <div>
-            <button onClick={handleDelete}>X</button>
-            <p>{s.name}</p>
-            <p>{s.country}</p>
-            <p>{s.keyword}</p>
-            </div>
-            </div>
-          ))
-        }
         <button onClick={handleLogout}>Cerrar sesión</button>
       </div>
     </div>
