3319be996d553a8452ad59dfb1a4692fba6d23eb
V33RO <cardozoveronicaro@gmail.com>
2021-09-22 14:40:36 -0300

    listo

--- client/src/Components/Carrusel/Carrusel.jsx
+++ client/src/Components/Carrusel/Carrusel.jsx
@@ -1,29 +1,70 @@
-import React,{useState} from "react";
-import "./Carrusel.css"
-import {FaArrowAltCircleLeft, FaArrowAltCircleRight, FaSleigh} from "react-icons/fa"
-import { slides } from "./carruseldata";
 
+import "./Carrusel.css"
+import { Link } from 'react-router-dom';
+import React,{ useEffect} from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {getNews} from '../../actions/actions';
 import Slider from 'infinite-react-carousel';
 
+
+const{ REACT_APP_BACK_BASE_URL} = process.env
+
 const Carrusel = () => {
-    return (
-      <Slider autoplay={true} autoplaySpeed={3000} dots>
+
+  const allNews = useSelector((state) => state.news);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getNews());
+  }, [dispatch]);
+
+
+var imagenes=[];
+var idnoticia=[];
+var titulo=[];
+for(var i=0;i<allNews.length;i++){
+     imagenes.push(allNews[i].image)
+     idnoticia.push(allNews[i].id)
+     titulo.push(allNews[i].title)
+}
+
+  return(
+
+    <Slider autoplay={true} autoplaySpeed={3000} dots>
       <div className='slider'>
-      <img src={slides[0].image} alt="travel image" className="image"/>
+        <Link to={"/foronoticias/" + idnoticia[0]}>
+         {/* eslint-disable-next-line */}
+        <img src={`${REACT_APP_BACK_BASE_URL}/` + imagenes[0]} alt="travel image" className="image"/>
+         {/* eslint-disable-next-line */} 
+        <a>{titulo[0]}</a>
+        </Link>
       </div>
       <div>
-      <img src={slides[1].image} alt="travel image" className="image"/>
+      <Link to={"/foronoticias/" + idnoticia[1]}>
+        {/* eslint-disable-next-line */}
+        <img src={`${REACT_APP_BACK_BASE_URL}/` + imagenes[1]} alt="travel image" className="image"/>
+         {/* eslint-disable-next-line */} 
+        <a>{titulo[1]}</a>
+        </Link>
       </div>
       <div>
-      <img src={slides[2].image} alt="travel image" className="image"/>
+      <Link to={"/foronoticias/" + idnoticia[2]}>
+        {/* eslint-disable-next-line */}
+        <img src={`${REACT_APP_BACK_BASE_URL}/` + imagenes[2]} alt="travel image" className="image"/>
+         {/* eslint-disable-next-line */} 
+        <a>{titulo[2]}</a>
+        </Link>
       </div>
       <div>
-      <img src={slides[3].image} alt="travel image" className="image"/>
-      </div>
-      <div>
-      <img src={slides[4].image} alt="travel image" className="image"/>
+      <Link to={"/foronoticias/" + idnoticia[3]}>
+         {/* eslint-disable-next-line */}
+         <img src={`${REACT_APP_BACK_BASE_URL}/` + imagenes[3]} alt="travel image" className="image"/>
+         {/* eslint-disable-next-line */} 
+        <a>{titulo[3]}</a>
+        </Link>
       </div>
     </Slider>
-      );
+
+  )
     };
 export default Carrusel;
--- client/src/Components/ForoNoticias/ForoNoticias.jsx
+++ client/src/Components/ForoNoticias/ForoNoticias.jsx
@@ -4,17 +4,14 @@ import {getCommentNotice,getNewsById,postCommentNotice } from '../../actions/act
 import {useParams} from 'react-router-dom';
 import "./ForoNoticias.css";
 import style from "./Comment.css";
-const{ REACT_APP_BACK_BASE_URL} = process.env
+
 
 
 export default function Foro(props) {
   // eslint-disable-next-line 
   const {id} = useParams();
-
- 
+  const idUser = useSelector((state) => state.userId)
   
-console.log("idnotice",id)
-
  function getCurrentDate(separator='-'){
 
     let newDate = new Date()
@@ -26,7 +23,7 @@ console.log("idnotice",id)
     }
 const fecha = getCurrentDate();
 // eslint-disable-next-line 
-let [stateComment, setStateComment] = useState([]);
+let [stateComment, setStateComment] = useState(1);
 // eslint-disable-next-line 
 let [stateNewId, setStateNew] = useState([]);
 // eslint-disable-next-line 
@@ -34,8 +31,9 @@ let [stateNewId, setStateNew] = useState([]);
 const dispatch = useDispatch();
 // eslint-disable-next-line
 
+
 useEffect(() => {
-  dispatch(getCommentNotice())
+  dispatch(getCommentNotice(id))
 },
   // eslint-disable-next-line
   [stateComment]);
@@ -49,9 +47,8 @@ useEffect(() => {
   [stateNewId]);
 
   const getnews = useSelector((state) => state.statenewsid);
-  const Loggin = useSelector((state) => state.isLogged);
-  const noticeId=1;
-  const userId=1;
+  const Loggin = localStorage.getItem("isLogged")
+
 
   const [input, setInput] = useState({
        description:"",
@@ -64,12 +61,19 @@ useEffect(() => {
     })
   
 }
-
    async function handleSubmit(e) {
        e.preventDefault();
        console.log("guarda",input)
-       if(Loggin){
-        dispatch(postCommentNotice({ ...input},userId,noticeId))
+       if(Loggin==="true"){
+        dispatch(postCommentNotice({ ...input},idUser,id))
+        setInput({
+          ...input,
+          description:""
+         })
+         setStateComment({
+          stateComment:2
+         })
+
        }else{
          alert("Por favor logueate!")
        }
@@ -83,7 +87,7 @@ useEffect(() => {
       {
             getnews.length>0 ?
             <div>
-               <img   className='imgNotamujeres' src= { `${REACT_APP_BACK_BASE_URL}/` + getnews[0].image}   alt="no se encuentra la imagen" />
+               <img className='imgNotamujeres' src= { `http://localhost:3001/${getnews[0].image}` }   alt="no se encuentra la imagen" />
                <div className='tittleydate'>
                <div className='titulonoti'> { getnews[0].title}</div>
                <p className='date'> { getnews[0].date}</p>   
@@ -95,12 +99,6 @@ useEffect(() => {
         }
  
       <div>
-        <button className="btnnotianterior" type="submit">
-          Entrada anterior
-        </button>
-        <button className="btnnotisiguiente" type="submit">
-          Entrada siguiente
-        </button>
       </div>
       <div className="comentariosforo">
         Comentarios
