226933e7b143d4909348e323f406857c0c6df93f
Florencia Ferrari <69319724+FlorFerrari@users.noreply.github.com>
2021-09-23 21:36:54 -0300

    estilos_cargar_noticia (#240)

--- client/src/Pages/CargarNoticia.jsx
+++ client/src/Pages/CargarNoticia.jsx
@@ -1,10 +1,10 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import ImageContainer from "../Components/ImageContainer";
 import ImageForm from "../Components/ImageForm";
 import axios from "axios";
 import swal from "sweetalert";
 import "./CargarNoticia.css";
-const{ REACT_APP_BACK_BASE_URL} = process.env
+const { REACT_APP_BACK_BASE_URL } = process.env
 
 
 export function validate(input) {
@@ -19,7 +19,7 @@ export function validate(input) {
 }
 
 const CargarNoticia = () => {
-   
+
     const [newImage, setNewImage] = useState([]);
     const [input, setInput] = useState({
         title: "",
@@ -31,10 +31,10 @@ const CargarNoticia = () => {
     // eslint-disable-next-line
     const [errors, setErrors] = useState({})
 
-    
+
     const handleInputChange = (e) => {
         setInput({
-            ...input, 
+            ...input,
             [e.target.name]: e.target.value
         })
         setErrors(validate({
@@ -43,7 +43,7 @@ const CargarNoticia = () => {
         }));
     };
 
-    
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         let image2 = localStorage.getItem("image")
@@ -52,14 +52,14 @@ const CargarNoticia = () => {
             ...input,
             [e.target.name]: e.target.value
         }));
-        
+
         if (!errors.title && !errors.content && image2) {
-                await axios.post(`${REACT_APP_BACK_BASE_URL}/newNotice`, {input, image2, id});
-                swal("La noticia se creó correctamente", "Gracias", "success");    
-                setInput({
-                    title: "",
-                    content: "",
-                })
+            await axios.post(`${REACT_APP_BACK_BASE_URL}/newNotice`, { input, image2, id });
+            swal("La noticia se creó correctamente", "Gracias", "success");
+            setInput({
+                title: "",
+                content: "",
+            })
         } else {
             swal("Oh oh, algo salió mal", "Inténtelo nuevamente", "warning")
         }
@@ -69,17 +69,30 @@ const CargarNoticia = () => {
         setNewImage([...newImage, "New image"])
     }
     return (
-        <div>
-            <h1>Cargar noticia</h1>
+        <div className="cargar-noticia-container">
+            <div className="titulo-cargar-noticia">CARGAR NOTICIA</div>
+            <div className="cargarnoticialine"></div>
             <form className="back">
-                <input name="title" value={input.title} onChange={handleInputChange} placeholder="Título"></input>
-                { errors.title && <p>{errors.title}</p>}
-                <textarea name="content" value={input.content} onChange={handleInputChange} placeholder="Contenido"></textarea>
-                { errors.content && <p>{errors.content}</p>}
+                <div className="titulo-cargar">Título</div>
+                <input name="title" className="cargar-titulo-noticia" value={input.title} onChange={handleInputChange} placeholder="Título"></input>
+                {errors.title && <p>{errors.title}</p>}
+                <div className="titulo-cargar">Contenido</div>
+                <textarea className="cargar-contenido-noticia" name="content" value={input.content} onChange={handleInputChange} placeholder="Contenido"></textarea>
+                {errors.content && <p>{errors.content}</p>}
             </form>
-            <ImageContainer newImage={newImage} />
-            <ImageForm handleNewImage={handleNewImage}/>
-            <button type="submit" onClick={handleSubmit}>Crear</button>
+
+            <div className="cargaunaimagen">
+                <ImageContainer newImage={newImage} />
+            </div>
+
+            <div className="cargaunaimagen2">
+                <ImageForm handleNewImage={handleNewImage} />
+            </div>
+
+            <div className="boton-crearnoticia">
+                <button type="submit" onClick={handleSubmit}>Crear noticia</button>
+            </div>
+
         </div>
     )
 }
