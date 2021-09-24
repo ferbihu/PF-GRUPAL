d7da5c31620bec8036b585fc03c1e778ae7dad50
V33RO <cardozoveronicaro@gmail.com>
2021-09-24 12:19:32 -0300

    estiloscomentnoti (#256)
    Co-authored-by: Mora Bessone <79546017+morabessone@users.noreply.github.com>

--- client/src/Components/ForoNoticias/ForoNoticias.jsx
+++ client/src/Components/ForoNoticias/ForoNoticias.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { getCommentNotice, getNewsById, postCommentNotice } from '../../actions/actions.js';
 import { useParams } from 'react-router-dom';
 import "./ForoNoticias.css";
-import style from "./Comment.css";
+import style from "./Comment.module.css"
 const { REACT_APP_BACK_BASE_URL } = process.env
 
 
@@ -132,20 +132,43 @@ export default function Foro(props) {
         placeholder="Qué te pareció la noticia?"
         onChange={(e) => handleChange(e)}
       />
+      <div className="contenedorbot">
       <button className="btnenviarcomentario" type="submit" onClick={(e) => handleSubmit(e)}>
         Envíar
       </button>
-      <div open>
-        <summary className="btnmascomentarios">
-          mas comentarios
-        </summary>
-        {allcomment.map((e, i) => (
-          <p>
-            {e.date}
-            {e.description}
-          </p>
-        ))}
-      </div>
+ <div className={style.sty}>
+                <svg xmlns="http://www.w3.org/2000/svg">
+                    <symbol viewBox="0 0 24 24" id="expand-more">
+                        <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"/><path d="M0 0h24v24H0z" fill="none"/>
+                    </symbol>
+                    <symbol viewBox="0 0 24 24" id="close">
+                    <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/><path d="M0 0h24v24H0z" fill="none"/>
+                    </symbol>
+                 </svg>
+             </div>
+             <details open>
+                <summary className="btnmascomentarios">
+                    Mas comentarios
+                    <svg className={style.controlicon && style.controliconexpand} width="24" height="24" role="presentation"></svg>
+                    <svg className={style.controlicon && style.controliconclose} width="24" height="24" role="presentation"></svg>
+                </summary>
+                 {allcomment.map((e, i) => (
+       <div className="messages"> 
+      <div className="message-box">
+                                        {/* eslint-disable-next-line */} 
+                                           
+                    <div className="fecha">{e.date}</div>
+                    <div className="message-content">
+                    <div className="otro">{e.description}</div>
+              </div>
+        </div>
+        </div> 
+            ))}
+              </details>
     </div>
+
+      </div>
+     
+     
   );
 }
