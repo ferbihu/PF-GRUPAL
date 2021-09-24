f5b4032b2b7b18714b5409353da9c5c8040b8892
Florencia Ferrari <69319724+FlorFerrari@users.noreply.github.com>
2021-09-24 09:20:41 -0300

    estilos (#247)

--- client/src/Components/ForoNoticias/FailedSearch.jsx
+++ client/src/Components/ForoNoticias/FailedSearch.jsx
@@ -1,71 +1,76 @@
 import "./FailedSearch.css";
 import { Link } from 'react-router-dom';
-import React,{ useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {getNews} from '../../actions/actions';
+import { getNews } from '../../actions/actions';
 
 export default function FailedSearch() {
 
 
   const [inputSearch, setInputSearch] = useState({
-    title:""
-})
-  
+    title: ""
+  })
+
   function handleChange(e) {
     setInputSearch({
-        ...inputSearch,
-        title: e.target.value
+      ...inputSearch,
+      title: e.target.value
     })
   }
-    const allN = useSelector((state) => state.news);
+  const allN = useSelector((state) => state.news);
 
 
-    const dispatch = useDispatch();
-  
-    useEffect(() => {
-       // eslint-disable-next-line
-      dispatch(getNews());
-       // eslint-disable-next-line
-    }, [dispatch]);
-  
-  let arrayfilter=[];
- // eslint-disable-next-line
-   allN.map((nuevo)=>{
-         if(nuevo.title.includes(inputSearch.title)){
-          arrayfilter.push(nuevo);
-         }
-    })
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // eslint-disable-next-line
+    dispatch(getNews());
+    // eslint-disable-next-line
+  }, [dispatch]);
+
+  let arrayfilter = [];
+  // eslint-disable-next-line
+  allN.map((nuevo) => {
+    if (nuevo.title.includes(inputSearch.title)) {
+      arrayfilter.push(nuevo);
+    }
+  })
 
   return (
-    <div classeName="proyectocontainer">
-      <div className="tittleforo">Foro</div>
-      <div className="foroerror-line"></div>
-      <div className='error'>Lo sentimos su busqueda no produjo resultados</div>
-      <div>
-      <button className='btnsearch'>Buscar</button>
+    <>
+      <div className="tittleforo"></div>
+      <div className='error'>Lo sentimos, su busqueda no produjo resultados</div>
+      <div className="contenedor-error">
+
         <input
           className="inputsearch"
           autoComplete="off"
           type="text"
           name="description"
-          placeholder="Noticia"
+          placeholder="Buscador de noticias"
           value={inputSearch.title}
           onChange={(e) => handleChange(e)}
         />
-         {
-          arrayfilter?
-                  <Link to={"/searchnews/"+inputSearch.title}>
-        <button className='btnbuscar' type="submit">Buscar
-        </button>
-        </Link>
-          :
-          <Link to={"/failedsearch"}>
-          <button className='btnbuscar' type="submit">Buscar
-          </button>
-          </Link>
+        {
+          arrayfilter ?
+            <div className="boton-buscar">
+              <Link to={"/searchnews/" + inputSearch.title}>
+                <button className='btnbuscar' type="submit">Buscar
+                </button>
+              </Link>
+            </div>
+
+            :
+            <div className="boton-buscar">
+              <Link to={"/failedsearch"}>
+                <button className='btnbuscar' type="submit">Buscar
+                </button>
+              </Link>
+            </div>
+
         }
 
       </div>
-    </div>
+    </>
   );
 }
