6a20687e19b82d456bdcf696af0e96c5c048c39d
Florencia Ferrari <69319724+FlorFerrari@users.noreply.github.com>
2021-10-13 10:18:14 -0300

    correciones_css_sidebar (#264)

--- client/src/Components/Sidebar/Sidebar.jsx
+++ client/src/Components/Sidebar/Sidebar.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { FaPhoneAlt } from "react-icons/fa";
 import { HiLocationMarker } from "react-icons/hi"
 import { Link } from "react-router-dom";
- 
+
 import "react-pro-sidebar/dist/css/styles.css";
 import "./Sidebar.css";
 
@@ -16,9 +16,9 @@ import PopupsComment from "./PopUpComent";
 
 export default function Sidebar({ id, name, telephone, street, number, keyword, handleMarkerClick }) {
 
- 
+
   const [menuCollapse, setMenuCollapse] = useState(false)
-  
+
   const dispatch = useDispatch();
 
 
@@ -35,8 +35,8 @@ export default function Sidebar({ id, name, telephone, street, number, keyword, 
   const filterId = comments.filter((c) => c.safePlaceId === id)
 
   useEffect(() => {
-      dispatch(showCommentsSafePlaces(id))
-    }, 
+    dispatch(showCommentsSafePlaces(id))
+  },
     // eslint-disable-next-line
     [showCommentsSafePlaces])
 
@@ -56,63 +56,63 @@ export default function Sidebar({ id, name, telephone, street, number, keyword, 
 
   return (
     <>
-    <div>
-      <div className="scroll-side">
-        <div  className="contenedorsidebar" collapsed={menuCollapse}>
-            <div className="logotext">
-              <button className="btnSide" onClick={() => handleMarkerClick(false)}>x</button>
-              <p className="princip">{name}</p>
-              <div className="titleLine"></div>
-            </div>
-            <div className="closemenu" onClick={menuIconClick}>
-              {/* changing menu collapse icon on click */}
-            
-            </div>
-            <br />
-            <br />
-            <div className="txtyicon">
-            <FaPhoneAlt className="icono"/> 
-            <h3 className="txt1">{telephone}</h3>
-            </div>
-            <br />
-            <div className="txtyicon2">
-            <HiLocationMarker  className="icono"/>
-            <h3 className="txt1">{street} {number}</h3>
-            </div>
-            <div className="cont">
-              <h2 className="kw">Palabra clave:</h2>
-              <h2 className="keyword">{keyword}</h2>
-            </div>
-            {logeado === "true" ? 
-              <div className="cont-denuncia">
-              <div className="sidebar-align">
-              <h3 className="denunciaTit"> Este no es un lugar seguro?</h3>
-              <button className="denuncia" type="submit" disabled={state_popup2 === true || state_popup_warning2 === true} onClick={() => HandleWarningClick()} >Denuncialo</button>
-            </div></div> : <div className="denuncia"><Link to="/iniciasesion">  Iniciá sesión para denunciar el lugar</Link></div>}
+      <div className="contenedorsidebar" collapsed={menuCollapse}>
+        <div className="logotext">
+          <button className="btnSide" onClick={() => handleMarkerClick(false)}>x</button>
+          <p className="princip">{name}</p>
+          <div className="titleLine"></div>
+        </div>
+        <div className="closemenu" onClick={menuIconClick}>
+          {/* changing menu collapse icon on click */}
 
-            {logeado === "true" ? 
-            <div className="cont-res">
-            <button className="btnRes" type="submit" disabled={state_popup2 === true || state_popup_warning2 === true} onClick={() => HandleCommentClick()}>Dejar una reseña</button>
-            </div>
-              : <div className="comentario"><Link to="/iniciasesion">Iniciá sesión para dejar tu reseña!</Link></div>}
-                      <div>
-                        <div className="comm-scrll">
+        </div>
+        <br />
+        <br />
+        <div className="txtyicon">
+          <FaPhoneAlt className="icono" />
+          <h3 className="txt1">{telephone}</h3>
+        </div>
+        <br />
+        <div className="txtyicon2">
+          <HiLocationMarker className="icono" />
+          <h3 className="txt1">{street} {number}</h3>
+        </div>
+        <div className="cont">
+          <h2 className="kw">Palabra clave:</h2>
+          <h2 className="keyword">{keyword}</h2>
+        </div>
+
+
+
+        <div>
+          <div className="comm-scrll">
             {
-                filterId?.map((c) => {
-                    return (
-                      <div>
-                      <div key={c.id} value={c.id} className="commentsCont">
-                        <p key={c.id} value={c.id} className="conte">{c.comment_text}</p>
-                      </div>
-                      </div>
-                    )
-                  })
-                }
-                </div>
-        </div>
+              filterId?.map((c) => {
+                return (
+                  <div>
+                    <div key={c.id} value={c.id} className="commentsCont">
+                      <p key={c.id} value={c.id} className="conte">{c.comment_text}</p>
+                    </div>
+                  </div>
+                )
+              })
+            }
+
+          </div>
+          {logeado === "true" ?
+            <div className="cont-res">
+              <button className="btnRes" type="submit" disabled={state_popup2 === true || state_popup_warning2 === true} onClick={() => HandleCommentClick()}>Dejar una reseña</button>
+            </div>
+            : <div className="comentario"><Link to="/iniciasesion">Iniciá sesión para dejar tu reseña!</Link></div>}
+
+          {logeado === "true" ?
+            <div className="cont-denuncia">
+              <h3 className="denunciaTit"> Este no es un lugar seguro?</h3>
+              <button className="denuncia-boton" type="submit" disabled={state_popup2 === true || state_popup_warning2 === true} onClick={() => HandleWarningClick()} >Denuncialo</button>
+            </div> : <div className="denuncia"><Link to="/iniciasesion">  Iniciá sesión para denunciar el lugar</Link></div>}
         </div>
       </div>
-      </div>
+
     </>
 
   );
